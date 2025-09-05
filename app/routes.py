11748f3266c70439dfb90903a866aaa7cfdf68f9
kyle strougo <kstrougo@gmail.com>
2025-09-05 11:55:40 -0400

    Your commit message here

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,24 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request, flash
 from app import app
 from app.forms import LoginForm
 from datetime import datetime
+import os
+from werkzeug.utils import secure_filename
 
-@app.route('/')
+CORRECT_PASSWORD = "pass"
+
+# Configurations
+UPLOAD_FOLDER = 'app/static/uploads'   # folder where files will be saved
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Make sure upload folder exists
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/index')
 def index():
     # Example posts
@@ -35,11 +50,30 @@ def index():
 
     return render_template('index.html', title='Home', posts=posts, user_colors=user_colors)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/')
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
+    if request.method == "POST":
+        password = request.form.get("password")
+        if password == CORRECT_PASSWORD:
+            return redirect(url_for("index"))
+        else:
+            flash("Enter the correct password")
+            return redirect(url_for("login"))
+    return render_template("login.html")
 
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
 
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    filename = None
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        if file.filename == '':
+            return 'No selected file'
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('post.html', filename=filename)
+    return render_template('post.html', filename=filename)
