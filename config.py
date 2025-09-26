28f0b449a5cf6f06fa697c9a993e7fed758ced4d
kyle strougo <kstrougo@gmail.com>
2025-09-26 12:42:35 -0400

    Your commit message here

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
     ##SERVER_NAME = '192.168.1.117:5000' # Replace with your server's hostname and port
-    SERVER_NAME = '192.168.1.159:5000' # Replace with your server's hostname and port
+    ##SERVER_NAME = '192.168.1.159:5000' # Replace with your server's hostname and port
 
     APPLICATION_ROOT = '/' # Set if your app is hosted under a subpath
     PREFERRED_URL_SCHEME = 'https'
